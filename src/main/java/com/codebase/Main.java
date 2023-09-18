79f3a5127f42a85a13ed145dbe2a6a64e5c5e681
John <ashton.basile@gmail.com>
2023-09-18 15:44:56 +0100

    It is now working.

--- src/main/java/com/codebase/Main.java
+++ src/main/java/com/codebase/Main.java
@@ -3,7 +3,7 @@ package com.codebase;
 public class Main {
     public static void main(String[] args) {
         Planet mars = new Planet("Mars", 908973);
-        System.out.printf("%s is %.2f", mars.getName(), mars.getSize());
+        System.out.printf("%s is %.2f\n", mars.getName(), mars.getSize());
         System.out.println(mars.explode());
     }
 }
